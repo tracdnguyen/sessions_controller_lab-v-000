a319310fface2b550e18e9af8846442614e3f339
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-24 03:55:57 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,10 @@
 class SessionsController < ApplicationController
-
+  def create
+    if session[:name].nil?
+      redirect_to '/login'
+    else  
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
 end
